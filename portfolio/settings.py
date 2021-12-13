82eaea35cb4baf648789114b24de25d539d870d8
adrianpileaa <90518975+adrianpileaa@users.noreply.github.com>
2021-12-13 16:22:19 +0200

    Last

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -121,16 +121,16 @@ USE_TZ = True
 import os
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
