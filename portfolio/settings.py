65a64bbba0dcd1d3240bd8c1898d4bc76f4f24cd
adrianpileaa <90518975+adrianpileaa@users.noreply.github.com>
2021-12-13 15:05:15 +0200

    Final commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*idvdpep640mh1k)3i$nv94wfus)s4(td4mnfw7d4=z&)=27i+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['adrian-pilea-portfolio.herokuapp.com', '127.0.0.1:8000']
 
 
 # Application definition
